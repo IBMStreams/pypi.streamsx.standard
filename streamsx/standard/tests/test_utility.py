5fdb80529c0daed3aeedc1b9c0f48906fe271497
Dan Debrunner <debrunne@us.ibm.com>
2017-12-18 11:56:38 -0800

    Add pair/merge functions

--- streamsx/standard/tests/test_utility.py
+++ streamsx/standard/tests/test_utility.py
@@ -1,12 +1,64 @@
 from unittest import TestCase
+import time
 
 
 import streamsx.standard.utility as U
 
 from streamsx.topology.topology import Topology
 from streamsx.topology.tester import Tester
+from streamsx.topology.schema import StreamSchema
 
-class TestBeacon(TestCase):
+from streamsx.spl.op import Expression
+
+class PairCheck(object):
+    def __init__(self):
+        self.first = True
+        self.seq = 0
+    def __call__(self, t):
+        if self.first:
+            ok = self.seq == t['seq'] and 1.0 == t['score']
+            self.first = False
+        else:
+            ok = self.seq == t['seq'] and 2.0 == t['score']
+            self.first = True
+            self.seq += 1
+        return ok
+
+# input before split
+# {0,7}, {1,8}, {3,9}
+#
+# port 0 -> {1,14}, {2,16}, {0, 18}
+# port 1 -> {0,21}, {1,24}, {2, 27}
+#
+class PairMatchedCheck(object):
+    def __init__(self):
+        self.last = None
+        self.seen = [0, 0, 0]
+        self.p0 = [18, 14, 16]
+        self.p1 = [21, 24, 27]
+    def __call__(self, t):
+        print("DDDD", t, flush=True)
+        id_ = int(t['id'])
+        if self.last is not None:
+            if self.last != id_:
+                return False
+
+        if self.seen[id_] == 0:
+            ok = self.p0[id_] == t['v']
+            self.seen[id_] = 1
+            self.last = id_
+            return ok
+
+        if self.seen[id_] == 1:
+            ok = self.p1[id_] == t['v']
+            self.seen[id_] = 2
+            self.last = None
+            return ok
+
+        return False
+            
+
+class TestUtility(TestCase):
     def setUp(self):
         Tester.setup_standalone(self)
      
@@ -19,10 +71,6 @@ class TestBeacon(TestCase):
         tester.tuple_count(s, 122)
         tester.test(self.test_ctxtype, self.test_config)
 
-
-class TestThreadedSpit(TestCase):
-    def setUp(self):
-        Tester.setup_standalone(self)
 
     def test_spray(self):
         topo = Topology()
@@ -38,3 +86,43 @@ class TestThreadedSpit(TestCase):
         tester.test(self.test_ctxtype, self.test_config)
 
     
+    def test_delay(self):
+        topo = Topology()
+        s = U.sequence(topo, iterations=223)
+        s = U.delay(s, delay=0.4)
+        
+        tester = Tester(topo)
+        tester.tuple_count(s, 223)
+        tester.tuple_check(s, lambda t : (time.time() - t['ts'].time()) > 0.35)
+        tester.test(self.test_ctxtype, self.test_config)
+
+    def test_pair(self):
+        topo = Topology()
+        s = U.sequence(topo, iterations=932)
+        rschema = U.SEQUENCE_SCHEMA.extend(StreamSchema('tuple<float64 score>'))
+        r0 = s.map(lambda t : (t['seq'], t['ts'], 1.0), schema=rschema)
+        r1 = s.map(lambda t : (t['seq'], t['ts'], 2.0), schema=rschema)
+
+        r = U.pair(r0, r1)
+
+        tester = Tester(topo)
+        tester.tuple_count(r, 932*2)
+        tester.tuple_check(r, PairCheck())
+        tester.test(self.test_ctxtype, self.test_config)
+
+    def test_pair_matched(self):
+        topo = Topology()
+        s = topo.source([0, 1, 2])
+        rs = 'tuple<rstring id, int32 v>'
+        s = s.map(lambda t : (str(t),t + 7), schema=rs)
+        
+        # "Reorder" tuples as well
+        r0 = s.map(lambda t : ((int(t['id']) + 1)%3, t['v'] * 2), schema=rs)
+        r1 = s.map(lambda t : (t['id'], t['v'] * 3), schema=rs)
+
+        r = U.pair(r0, r1, matching='id')
+
+        tester = Tester(topo)
+        tester.tuple_count(r, 6)
+        tester.tuple_check(r, PairMatchedCheck())
+        tester.test(self.test_ctxtype, self.test_config)
